448f9853aec4310a479be77a23e60aa21e90ccb0
Grazia Palombella <grazia.palombella@gmail.com>
2021-08-25 22:54:17 +0200

    git init + header to review

---
+++ vue-shop/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <Header />
+</template>
+
+<script>
+
+import Header from './components/Header.vue'
+export default {
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
+
+@import url('https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@300;400;700&family=Lato:wght@300;400;700&family=Spartan:wght@700&display=swap');
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#app {
+  font-family: 'Lato', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  
+  color: #2c3e50;
+
+}
+</style>
---
+++ vue-shop/src/components/Header.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="header__container">
+    <div class="header__container-inner">
+      <div class="header__content">
+        <h3>VueShop</h3>
+        <ul>
+          <li><a href="#" class="link">Products</a></li>
+          <li><a href="#" class="link">Cart</a></li>
+        </ul>
+        <a class="login">  Login</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+
+};
+</script>
+
+
+<style scoped>
+
+.header__content {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 1rem 1.7rem;
+}
+
+h3 {
+  font-size: 2rem;
+  font-weight: 700 ;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+  font-size: 1.7rem;
+  font-weight: 700;
+  text-decoration: underline;
+  text-decoration-color: transparent;
+
+}
+
+
+
+
+
+a:hover {
+  text-underline-offset: 0.25em;
+  text-decoration-thickness: 3px;
+  transition: all 0.3s ease-in-out;
+  cursor: pointer;
+  text-decoration-color: #42b983;
+}
+
+.login {
+  color: #42b983;
+  font-size: 1.2rem;
+  padding: 8px 24px;
+
+  border: 2px solid #42b983;
+  border-radius: 18px;
+}
+</style>
